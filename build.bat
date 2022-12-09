c4e340661241b042b948ace1c32bd0713fb4d01e
donnyboi1234 <95443970+donnyboi1234@users.noreply.github.com>
2022-12-09 12:43:39 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 python --version 2>&1 | findstr " 3.11" >nul
 if %errorlevel% == 0 (
-    echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
+    echo python 3.11.x and up are not supported by repyrean. Please downgrade to python 3.10.x.
     pause
     exit
 )
